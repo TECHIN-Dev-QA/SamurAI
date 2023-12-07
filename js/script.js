a7785a1f1d198b7e15e44f00193d01a3345b1b1e
valen <valentinas.daujotas@gmail.com>
2023-12-07 17:00:38 +0200

    footer be medijos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let form = document.querySelector('.form_group'),
     formInputs = document.querySelectorAll('.js-input'),
-    inputCheckbox = document.querySelector('#privacy_check');
+    inputCheckbox = document.querySelector('#js-input-checkbox');
 
 function validateEmail(email) {
     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
